82820eebd55e94c72e1ac1a58ebf9114769c8ef1
DuncanMilrad <123710943+DuncanMilrad@users.noreply.github.com>
2023-01-28 00:46:35 -0800

    Add files via upload

--- src/ArbitrageBetting/Main.java
+++ src/ArbitrageBetting/Main.java
@@ -1,11 +1,25 @@
 package ArbitrageBetting;
 
+import Scraper.*;
+import API.*;
+import java.io.IOException;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Calculator test = new Calculator("Betonline", "Bovada", "N/A", "N/A", 100);
+		// Calculator test = new Calculator("Betonline", "Bovada", "N/A", "N/A", 100);
+
+		// Scraper test1 = new Scraper();
+		// test1.scrape("https://www.betonline.ag/sportsbook");
+
+		// System.out.println(test.calculate(2, 2.2, 100));
+
+		ApiRequest test = new ApiRequest("betonlineag", "6e132c8365e1f407ce06107cb47ddfea");
 		
-		System.out.println(test.calculate(2, 2.2, 100));
+		test.getOdds("basketball_nba");
+
+		//System.out.println("Testing 2 - Send Http POST request"); test.sendPost();
+		 
 
 	}
 }
